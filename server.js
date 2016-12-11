ac8c008495d2f89f5f0c5541fca1d3666dc01925
Tashfeen Mustafa Choudhury <tashfeenc1234@gmail.com>
2016-12-11 18:42:19 +0000

    Second commit. Added code to parse and validate date string as per specs

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var moment = require('moment')
 
 var app = express()
 
@@ -11,7 +12,28 @@ app.get('/:id', function(request, response) {
     while( dateString.charAt( 0 ) === '/' )
     dateString = dateString.slice( 1 );
     
-    response.send(dateString)
+    //Check if dateString contains '%20'
+    if(dateString.includes("%20"))
+    {
+        // Replace '%20' with ' '
+        var naturalDate = dateString.split("%20").join(" ")
+        console.log("Here")
+        console.log(naturalDate)
+    }
+    
+    var unix = dateString
+    
+    // Check if dateString actually contains a timestamp in unix or natural language
+    var m = moment(unix, "x") || moment(naturalDate, "MMMM DD, YYYY")
+    
+    // Return null if not a timestamp
+    if(m.isValid)
+    {
+        response.send(dateString)
+    }    
+    else
+        response.send("invalid date format")
+    
     
 })
 
