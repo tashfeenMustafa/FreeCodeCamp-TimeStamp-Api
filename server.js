8daa42e0841be024ba74122093ffea308d892f4b
Tashfeen Mustafa Choudhury <tashfeenc1234@gmail.com>
2016-12-13 09:02:24 +0000

    Final commit. API built.

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ var moment = require('moment')
 
 var app = express()
 
-app.get('/:id', function(request, response) {
+app.get('/:id', function(request, response) 
+{
+    var naturalDate
+    var ifNatural
+    var ifUnix
+    
     var url = request.url
     var path = request.path
     
     var dateString = path;
-
+    
+    // If dateString contains '/' at index 0, remove it
     while( dateString.charAt( 0 ) === '/' )
     dateString = dateString.slice( 1 );
     
@@ -16,27 +22,48 @@ app.get('/:id', function(request, response) {
     if(dateString.includes("%20"))
     {
         // Replace '%20' with ' '
-        var naturalDate = dateString.split("%20").join(" ")
-        console.log("Here")
-        console.log(naturalDate)
+        naturalDate = dateString.split("%20").join(" ")
+        
+        // Check for naturalDate format
+        if(naturalDate)
+        {
+            ifNatural = moment(naturalDate, "MMMM DD, YYYY")
+        }    
+    }
+    else
+    {
+        // Check for unix timestamp format
+        ifUnix = moment(dateString, "X")   
     }
     
-    var unix = dateString
+    var unix, natural
     
-    // Check if dateString actually contains a timestamp in unix or natural language
-    var m = moment(unix, "x") || moment(naturalDate, "MMMM DD, YYYY")
-    
-    // Return null if not a timestamp
-    if(m.isValid)
+    if(ifUnix)
     {
-        response.send(dateString)
-    }    
+        // Convert to Natural Language Date
+        natural = ifUnix.format("MMMM DD, YYYY")
+        unix = dateString
+    }
+    else if(ifNatural)
+    {
+        unix = ifNatural.format("X")
+        natural = naturalDate
+    }
     else
-        response.send("invalid date format")
+    {
+        return null
+    }
     
+    var dateObj = {
+        unix: unix,
+        natural: natural
+    }
+    
+    return dateObj
     
 })
 
-app.listen('8080', function() {
+app.listen('8080', function() 
+{
     console.log('App listening on port 8080');
 })
